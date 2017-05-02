14cc5069b08c990554ea6ad85e4b8bd2393f393f
erose357 <erose357@gmail.com>
2017-05-02 09:10:13 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,24 @@
+#1. How will you know when the problem is solved? (Identify the big-picture goal)
+#  the customer service representative will be able to accept & respond to user input and the program will be able to end
+#2. How do you want to use the software? (Identity the "interface")
+# command line?
+#3. What's the (next-)most trivial possible case? (Identify the next small-picture goal)
+# listed below
+#4. How do we achieve this goal? (Sketch an algorithm using pseudocode)
+# listed below
+# PARAMETERS
+# initial ouput: "HELLO, THIS IS A GROCERY STORE!"
+# accept and analyze user input based on the following rules:
+#  - If you don't hit anything (just hit enter) response is: "HELLO?!"
+#  - questions with any lower-case letters the response is: "I AM HAVING A HARD TIME HEARING YOU."
+#  - questions in all upper-case letters the response is: "NO, THIS IS NOT A PET STORE"
+#  - the first input "GOODBYE!" response is "ANYTHING ELSE I CAN HELP YOU WITH?"
+#  - the second input "GOODBYE!" response is "THANK YOU FOR CALLING!" and the program exits
+
+#PSEUDOCODE
+# 1. write initial output and create a variable with user input
+# 2. if user input is just enter output "HELLO"
+# 3. if user input has any lower-case letters ouput "I AM HAVING A HARD TIME HEARING YOU."
+# 4. if user input is all upper-case letters output "NO, THIS IS NOT A PET STORE"
+# 5. first user input "GOODBYE!" output "ANYTHING ELSE I CAN HELP YOU WITH?"
+# 6. second user input "GOODBYE!" output "THANK YOU FOR CALLING!" and exit the program
